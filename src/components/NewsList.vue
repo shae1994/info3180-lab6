f3bb31393f0f4ed5d76b96b7af3882a19b403d84
shae1994 <juddyshaew@gmail.com>
2022-04-03 17:05:19 -0500

    Satistfactory Styling Achieved.

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,7 +1,13 @@
 <template>
+
     <ul class="news__list">
     <li v-for="article in articles"
-    class="news__item"> <img :src= article.urlToImage />{{ article.title }} <br> {{ article.description }}</li>
+    class="news__item">
+        <div  class="img-responsive"><img id="images" :src= article.urlToImage /></div>
+        <div class="text-styler">{{ article.title }}</div>
+        <div class="desc">{{ article.description }}</div>
+        </li>
+        
         </ul>
     
 </template>
@@ -31,3 +37,46 @@
     }
     };
 </script>
+
+<style>
+.news__list{
+    display:grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: auto;
+    grid-gap: 20px;
+    text-allign: justify;
+    padding-top: 5%;
+    padding-bottom: 10%;
+    
+    
+}
+.news__item{
+    border: 2px solid rgba(235, 235, 235, 0.64);
+    border-bottom: 6px solid green;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius:10px;
+    }
+
+
+#images{
+    width:100%;
+    height: 35vh;
+}
+
+ul{
+    list-style: none;
+}
+
+.text-styler{
+    padding: 7px;
+    font-weight: bold;
+    text-align: left;
+}
+
+.desc{
+    padding: 10px 10px;
+    text-align: left;
+}
+
+
+</style>
