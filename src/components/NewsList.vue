7d85e2afcdd8cef594e36047aae3f70007c1fbf9
shae1994 <juddyshaew@gmail.com>
2022-04-03 14:39:44 -0500

    Check Point 2.

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,14 +1,33 @@
 <template>
- <ul class="news__list">
- <li class="news__item">News item 1</li>
- <li class="news__item">News item 2</li>
- <li class="news__item">News item 3</li>
- </ul>
+    <ul class="news__list">
+    <li v-for="article in articles"
+    class="news__item"> <img :src= article.urlToImage />{{ article.title }} <br> {{ article.description }}</li>
+        </ul>
+    
 </template>
+
+
 <script>
-export default {
- data() {
- return {};
- },
-};
+    export default {
+    data() {
+    return { articles:[]};
+    },
+    created() {
+        let self = this;
+
+        fetch('https://newsapi.org/v2/top-headlines?country=us',
+    {
+        headers: {
+        Authorization: `Bearer  ${import.meta.env.VITE_NEWSAPI_TOKEN}`
+        }
+        })
+    .then(function(response) {
+    return response.json();
+    })
+    .then(function(data) {
+    console.log(data);
+    self.articles = data.articles;
+    });
+    }
+    };
 </script>
